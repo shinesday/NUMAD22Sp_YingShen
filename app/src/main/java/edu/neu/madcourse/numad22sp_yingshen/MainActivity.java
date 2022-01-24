64b8e835be0c0459f351d4d2f7d559f1604d34a4
shinesday <shinesday@gmail.com>
2022-01-23 18:47:14 -0800

    Add files via upload

---
+++ app/src/main/java/edu/neu/madcourse/numad22sp_yingshen/MainActivity.java
@@ -0,0 +1,30 @@
+package edu.neu.madcourse.numad22sp_yingshen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Ying Shen\nshen.ying@northeastern.edu",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+}
